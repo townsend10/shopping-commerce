f16219caa0e2a139dce3c6d123deffc02c44cdf2
george <jogos_townsend@hotmail.com>
2025-04-16 09:49:03 -0300

    add: styles for button,input,progress,textarea and global.css

--- README.md
+++ README.md
@@ -2,6 +2,8 @@ This is a [Next.js](https://nextjs.org) project bootstrapped with [`create-next-
 
 ## Getting Started
 
+##VIDEO: 38:45
+
 First, run the development server:
 
 ```bash
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,27 @@
 import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
 
 export default function Home() {
   return (
-    <div>
-      <Button>Hello</Button>
+    <div className="flex flex-col gap-y-4">
+      <div>
+        <Button variant={"elevated"}>Butao</Button>
+      </div>
+      <div>
+        <Input placeholder="input por" />
+      </div>
+      <div>
+        <Progress value={50} />
+      </div>
+      <div>
+        <Textarea placeholder="text area" />
+      </div>
+      <div>
+        <Checkbox />
+      </div>
     </div>
   );
 }
