499f19cb9b8a03b8b289326244e522b92c338161
george <jogos_townsend@hotmail.com>
2025-04-25 13:32:57 -0300

    fix: superjson active

--- src/trpc/client.tsx
+++ src/trpc/client.tsx
@@ -6,6 +6,7 @@ import { createTRPCClient, httpBatchLink } from "@trpc/client";
 import { createTRPCContext } from "@trpc/tanstack-react-query";
 import { useState } from "react";
 import { makeQueryClient } from "./query-client";
+import superjson from "superjson";
 import type { AppRouter } from "./routers/_app";
 export const { TRPCProvider, useTRPC } = createTRPCContext<AppRouter>();
 let browserQueryClient: QueryClient;
@@ -42,7 +43,8 @@ export function TRPCReactProvider(
     createTRPCClient<AppRouter>({
       links: [
         httpBatchLink({
-          // transformer: superjson, <-- if you use a data transformer
+          transformer: superjson,
+          //  <-- if you use a data transformer
           url: getUrl(),
         }),
       ],
--- src/trpc/query-client.ts
+++ src/trpc/query-client.ts
@@ -10,13 +10,12 @@ export function makeQueryClient() {
         staleTime: 30 * 1000,
       },
       dehydrate: {
-        // serializeData: superjson.serialize,
         shouldDehydrateQuery: (query) =>
           defaultShouldDehydrateQuery(query) ||
           query.state.status === "pending",
       },
       hydrate: {
-        // deserializeData: superjson.deserialize,
+        deserializeData: superjson.deserialize,
       },
     },
   });
